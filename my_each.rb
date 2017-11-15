b3f6058a0f6cd134c17dc79cd15ee422926c09af
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-15 16:57:22 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
 def my_each (array)
 counter=0
 
-  while counter<array.size do {|n|
+  while counter<array.size do |n|
     puts n
     yield(n)
-    counter +=1}
+    counter +=1
   end
   array
 end
